ee303f05e70aec2b72c06591947828d55a679f9c
noName <samyrize77777@gmail.com>
2025-06-02 12:33:41 +0400

    home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { supabase } from './supabase.js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SessionContextProvider>
+  </React.StrictMode>
+);
