0d424a497e025ea590977c678ec5fa19a111f95a
noName <samyrize77777@gmail.com>
2025-06-03 11:44:24 +0400

    styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,7 @@ const Home = () => {
   return (
     <div style={styles.container}>
       <h1 style={styles.header}>Сегодня</h1>
+      <h2 style={styles.header}>Упражнения</h2>
 
       <div style={{ marginTop: 20 }}>
         {cards.map((card) => (
